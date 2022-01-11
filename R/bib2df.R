3bda6833b17d24d38f5f25ae981d8a76ad3a18e6
xiangtuochen <97435396+xiangtuochen@users.noreply.github.com>
2022-01-11 13:57:35 +0800

    Update bib2df.R

--- R/bib2df.R
+++ R/bib2df.R
@@ -132,7 +132,7 @@ postprocessing <-function(DATA,dbsource){
     
     lastname <- trim(gsub(",.*","",l))
     firstname <- strsplit(trim(gsub(".*,","",l))," ")
-    firstname <- gsub("[^:A-Z:]","",firstname)
+    #firstname <- gsub("[^:A-Z:]","",firstname)
     AU <- paste(lastname,unlist(firstname),sep=" ",collapse=";")
     return(AU)
   })
